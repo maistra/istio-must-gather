f6b7312fd57f7d472ff2186c4a9ee2cda90349bb
Marko Lukša <marko.luksa@gmail.com>
2023-05-04 14:41:35 +0200

    OSSM-1647 Include all CSVs in operator namespace (#21)

--- gather_istio.sh
+++ gather_istio.sh
@@ -167,6 +167,8 @@ function main() {
 
   resources+="$(addResourcePrefix ns "${controlPlanes}")"
 
+  oc adm inspect "--dest-dir=${BASE_COLLECTION_PATH}" -n "${operatorNamespace}" clusterserviceversion
+
   for cp in ${controlPlanes}; do
       echo "Processing control plane ${cp}"
 
